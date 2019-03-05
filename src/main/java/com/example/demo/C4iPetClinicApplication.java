05bf4ea57d83c535f295c3f02be5bdba7998482f
Nitesh <niteshghumatkar@gmail.com>
2019-03-05 11:00:58 +0530

    spring project initialize

---
+++ src/main/java/com/example/demo/C4iPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4iPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4iPetClinicApplication.class, args);
+	}
+
+}
